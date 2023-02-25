9cfa0ca829edfd82e539502e8b1f606bc962abf9
Joe Webber <35155442+j-webber@users.noreply.github.com>
2023-02-25 10:23:47 -0600

    started added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "#14203E",
+        lightGold: "#FFFCF8",
+        mediumGold: "#F6F0E8",
+        darkGold: "#F4E8D8",
+      },
+      fontFamily: {
+        sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+        serif: ["PT Serif", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
