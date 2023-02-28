806e817d7f2f3bbf2e78c061e010cf45f12825b4
Joe Webber <35155442+j-webber@users.noreply.github.com>
2023-02-27 21:41:05 -0600

    c

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,39 @@
 //mobile resonsive hamburger menu
-const btn = document.getElementById("menu-btn");
+const menuBtn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
+const menuItem = document.querySelectorAll(".menu-item");
 
-btn.addEventListener("click", () => {
-  btn.classList.toggle("open");
+function menuToggle() {
+  menuBtn.classList.toggle("open");
   nav.classList.toggle("flex");
   nav.classList.toggle("hidden");
+}
+
+menuBtn.addEventListener("click", () => {
+  menuToggle();
+});
+
+menuItem.forEach((e) => {
+  e.addEventListener("click", () => {
+    menuToggle();
+  });
+});
+
+//log in
+const logInBtn = document.querySelectorAll(".log-in-btn");
+const logIn = document.getElementById("log-in");
+const closeLogInBtn = document.getElementById("close-log-in");
+
+function toggleLogIn() {
+  logIn.classList.toggle("hidden");
+}
+
+logInBtn.forEach((e) => {
+  e.addEventListener("click", () => {
+    toggleLogIn();
+  });
+});
+
+closeLogInBtn.addEventListener("click", () => {
+  toggleLogIn();
 });
